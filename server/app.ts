bf94e336bfb91eab24707f1c087e8bbbde253886
Tajeddine <tajeddine.js@hotmail.com>
2022-04-23 12:04:12 +0000

    Improved reusability

--- server/app.ts
+++ server/app.ts
@@ -6,9 +6,15 @@ import { RoutesV1 } from "./api/v1/routes/routes"
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(helmet)
+app.use(helmet())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/api", RoutesV1)
 
-export default app
+// * Server
+const port = process.env.APP_PORT || 3000
+app
+  .listen(port, (): void => {
+    console.log(`Server running on port ${port}`)
+  })
+  .on("error", (e: Error) => console.log(e))
